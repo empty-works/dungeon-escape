2fcce1d6da8b4d5d302ef74adf8f839c520d85b3
empty-works <pama.mark@gmail.com>
2021-01-02 22:00:05 +0100

    Had to create another global position component to the actors. Now the component returns the names and positions of all actors.

--- Source/DungeonEscape/GlobalPosition.cpp
+++ Source/DungeonEscape/GlobalPosition.cpp
@@ -1,11 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "WorldPosition.h"
-#include "GameFramework/Actor.h"
+#include "GlobalPosition.h"
+#include "GameFramework/Actor.h" 
 
 // Sets default values for this component's properties
-UWorldPosition::UWorldPosition()
+UGlobalPosition::UGlobalPosition()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -16,7 +16,7 @@ UWorldPosition::UWorldPosition()
 
 
 // Called when the game starts
-void UWorldPosition::BeginPlay()
+void UGlobalPosition::BeginPlay()
 {
 	Super::BeginPlay();
 
@@ -28,7 +28,7 @@ void UWorldPosition::BeginPlay()
 
 
 // Called every frame
-void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UGlobalPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
