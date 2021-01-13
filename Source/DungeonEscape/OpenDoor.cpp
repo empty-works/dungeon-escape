102375529b9e42e6144a893553c01d63667298ca
empty-works <pama.mark@gmail.com>
2021-01-13 10:34:12 +0100

    Accessing the yaw of the target actor.

--- Source/DungeonEscape/OpenDoor.cpp
+++ Source/DungeonEscape/OpenDoor.cpp
@@ -20,10 +20,7 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	float MyFloat = 90.f; // Same as 90.0f. 90.f matches Unreal documentation.
-	FRotator CurrentRotation = GetOwner()->GetActorRotation();
-	FRotator NewRotation(0.f, 90.f, 0.f);
-	GetOwner()->SetActorRotation(NewRotation);
+	
 }
 
 
@@ -32,6 +29,12 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
+	//UE_LOG(LogTemp, Warning, TEXT("%f"), *TargetYaw);
+
+	// float MyFloat = 90.f; // Same as 90.0f. 90.f matches Unreal documentation.
+	// FRotator CurrentRotation = GetOwner()->GetActorRotation();
+	// FRotator NewRotation(0.f, 90.f, 0.f);
+	// GetOwner()->SetActorRotation(NewRotation);
 }
 
